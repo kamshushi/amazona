a18a61de64d0b24880ba52d5f18523e99bfb7368
kamshushi <ahmed.elkamshoushy7@gmail.com>
2021-10-19 13:55:24 +0200

    5

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -2,15 +2,20 @@ import nc from 'next-connect';
 import Product from '../../models/Product';
 import db from '../../utils/db';
 import data from '../../utils/data';
+import User from '../../models/User';
 
 const handler = nc();
 
 handler.get(async (req, res) => {
   await db.connect();
-  //   Delete existing data
+  //   Delete existing data then add data to products
   await Product.deleteMany();
-  //   Add data
   await Product.insertMany(data.products);
+
+  // Users
+  await User.deleteMany();
+  await User.insertMany(data.users);
+
   await db.disconnect();
   res.send({ message: 'seeded successfully' });
 });
--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,9 @@
+import axios from 'axios';
+import NextLink from 'next/link';
+import React, { useState } from 'react';
+import Layout from '../components/Layout';
+import useStyles from '../utils/styles';
+// MUI
 import {
   List,
   ListItem,
@@ -6,35 +12,56 @@ import {
   Button,
   Link,
 } from '@material-ui/core';
-import NextLink from 'next/link';
-import React from 'react';
-import Layout from '../components/Layout';
-import useStyles from '../utils/styles';
 
 export default function Login() {
   const classes = useStyles();
+
+  // form state
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Submit handler
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      console.log(data);
+      alert('success login');
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
         <List>
           <ListItem>
             <TextField
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               variant="outlined"
               fullWidth
               id="email"
               label="Email"
+              // props to be applied to the <input /> element in the TextField
               inputProps={{ type: 'email' }}
             ></TextField>
           </ListItem>
           <ListItem>
             <TextField
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               variant="outlined"
               fullWidth
               id="password"
               label="Password"
+              // props to be applied to the <input /> element in the TextField
               inputProps={{ type: 'password' }}
             ></TextField>
           </ListItem>
